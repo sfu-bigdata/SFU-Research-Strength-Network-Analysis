873938cdb19c306ac3fadfd7569c983fc598ac0e
Curtis Grotzke <cugrotzke@gmail.com>
2025-05-16 15:09:47 -0700

    Basic endpoints

---
+++ etl.py
@@ -0,0 +1,10 @@
+'''
+etl.py
+ETL pipeline using Polars as a DataFrame library, connecting with the OpenAlex API
+'''
+
+import polars as pl
+from openalex_api import OpenAlexApi, APIEndpoints
+
+def extract():
+    pass
